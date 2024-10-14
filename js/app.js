3280bd038460168909c840236dea6c2f8a6f9e42
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-14 14:57:08 +0200

    add: dati da inserire da parte dell'utente e calcolo prezzo base

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// dati da inserire da parte dell'utente
+const kmPercorrere = parseInt(prompt('Inserire il numero di km da percorrere'))
+console.log(kmPercorrere)
+const etàPasseggiero = parseInt(prompt('Inserire età passeggiero'))
+console.log(etàPasseggiero)
+// calcolo prezzo base
+const prezzoBase = kmPercorrere * 0.21
+console.log(prezzoBase)
