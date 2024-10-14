4b1110ddcb6085a582c2c15a7b223a0457701911
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-14 16:13:27 +0200

    fix: nome variabile

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 // dati da inserire da parte dell'utente
 const kmPercorrere = parseInt(prompt('Inserire il numero di km da percorrere'))
 console.log('km:',kmPercorrere)
-const etàPasseggiero = parseInt(prompt('Inserire età passeggiero'))
-console.log('Anni:',etàPasseggiero)
+const etaPasseggiero = parseInt(prompt('Inserire età passeggiero'))
+console.log('Anni:',etaPasseggiero)
 const prezzoKm = 0.21
 // calcolo sconto
 let sconto = 0
-if (etàPasseggiero < 18) {
+if (etaPasseggiero < 18) {
     sconto = kmPercorrere * prezzoKm  * 20 / 100
-} else if (etàPasseggiero >= 65){
+} else if (etaPasseggiero >= 65){
     sconto = kmPercorrere * prezzoKm  * 40 / 100
 }
 // calcolo prezzo finale
