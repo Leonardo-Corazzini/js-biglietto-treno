bd10c9d4ed5881528df3aebbef162f204217aee4
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-14 15:18:36 +0200

    add : calcolo sconto e calcolo prezzo finale

--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@ console.log(etàPasseggiero)
 // calcolo prezzo base
 const prezzoBase = kmPercorrere * 0.21
 console.log(prezzoBase)
+// calcolo sconto
+let sconto = 0
+if (etàPasseggiero < 18) {
+    sconto = prezzoBase * 20 / 100
+} else if (etàPasseggiero >= 65){
+    sconto = prezzoBase * 40 / 100
+}
+// calcolo prezzo finale
+const prezzoFinale = prezzoBase - sconto
+console.log(prezzoFinale)
