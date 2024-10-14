f0ccf308fe08d41f40cd880f3b0741f2b55b8575
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-14 16:09:45 +0200

    add: ridotto linee di codice

--- js/app.js
+++ js/app.js
@@ -3,16 +3,14 @@ const kmPercorrere = parseInt(prompt('Inserire il numero di km da percorrere'))
 console.log('km:',kmPercorrere)
 const etàPasseggiero = parseInt(prompt('Inserire età passeggiero'))
 console.log('Anni:',etàPasseggiero)
-// calcolo prezzo base
-const prezzoBase = kmPercorrere * 0.21
+const prezzoKm = 0.21
 // calcolo sconto
 let sconto = 0
 if (etàPasseggiero < 18) {
-    sconto = prezzoBase * 20 / 100
+    sconto = kmPercorrere * prezzoKm  * 20 / 100
 } else if (etàPasseggiero >= 65){
-    sconto = prezzoBase * 40 / 100
+    sconto = kmPercorrere * prezzoKm  * 40 / 100
 }
 // calcolo prezzo finale
-const prezzoScontato = prezzoBase - sconto
-const prezzoFinale = prezzoScontato.toFixed(2)
+const prezzoFinale = (kmPercorrere * prezzoKm - sconto).toFixed(2)
 console.log('Prezzo:',prezzoFinale,'€')
