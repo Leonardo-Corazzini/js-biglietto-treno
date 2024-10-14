a855d7db8fb70415dbd31f9a05cf2c441be04737
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-14 15:43:36 +0200

    fix: prezzo finale

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 // dati da inserire da parte dell'utente
 const kmPercorrere = parseInt(prompt('Inserire il numero di km da percorrere'))
-console.log(kmPercorrere)
+console.log('km:',kmPercorrere)
 const etàPasseggiero = parseInt(prompt('Inserire età passeggiero'))
-console.log(etàPasseggiero)
+console.log('Anni:',etàPasseggiero)
 // calcolo prezzo base
 const prezzoBase = kmPercorrere * 0.21
-console.log(prezzoBase)
 // calcolo sconto
 let sconto = 0
 if (etàPasseggiero < 18) {
@@ -14,5 +13,6 @@ if (etàPasseggiero < 18) {
     sconto = prezzoBase * 40 / 100
 }
 // calcolo prezzo finale
-const prezzoFinale = prezzoBase - sconto
-console.log(prezzoFinale)
+const prezzoScontato = prezzoBase - sconto
+const prezzoFinale = prezzoScontato.toFixed(2)
+console.log('Prezzo:',prezzoFinale,'€')
