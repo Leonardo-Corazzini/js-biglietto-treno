ea65272442d0b8f234090ab591b91ceaccee277e
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-14 17:05:10 +0200

    fix: attivato messaggio di allert per ogni dato richiesto dall'utente

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 // dati da inserire da parte dell'utente
 const kmPercorrere = parseInt(prompt('Inserire il numero di km da percorrere'))
-console.log('km:',kmPercorrere)
-const etaPasseggiero = parseInt(prompt('Inserire età passeggiero'))
-console.log('Anni:',etaPasseggiero)
-const prezzoKm = 0.21
-if (isNaN(kmPercorrere) || isNaN(etaPasseggiero) ) {
+if (isNaN(kmPercorrere) ) {
     alert('Sono validi soltanto caratteri numerici')
 
 }
+console.log('km:',kmPercorrere)
+const etaPasseggiero = parseInt(prompt('Inserire età passeggiero'))
+if (isNaN(etaPasseggiero)){
+    alert('Sono validi soltanto caratteri numerici')
+}
+console.log('Anni:',etaPasseggiero)
+const prezzoKm = 0.21
 // calcolo sconto
 let sconto = etaPasseggiero < 18 ? kmPercorrere * prezzoKm  * 20 / 100
            : etaPasseggiero > 65 ? kmPercorrere * prezzoKm  * 40 / 100
